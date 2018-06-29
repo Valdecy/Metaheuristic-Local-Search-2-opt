1e88fc521b295a3d59dc186194a843f092dc4596
Valdecy Pereira <valdecypereira@yahoo.com.br>
2018-06-29 18:57:21 -0300

    Update Python-MH-Local Search-2-opt.py

--- Python-MH-Local Search-2-opt.py
+++ Python-MH-Local Search-2-opt.py
@@ -16,7 +16,7 @@ import pandas as pd
 import copy
 
 # Function: 2_opt
-def local_search_2_opt(Xdata, city_tour, full_swap = True, recursive_seeding = 1):
+def local_search_2_opt(Xdata, city_tour, recursive_seeding = 1):
     count = 0
     city_list = copy.deepcopy(city_tour)
     while (count < recursive_seeding):
@@ -25,15 +25,7 @@ def local_search_2_opt(Xdata, city_tour, full_swap = True, recursive_seeding = 1
         seed = copy.deepcopy(city_list)        
         for i in range(0, len(city_list[0]) - 2):
             for j in range(i, len(city_list[0]) - 1):
-                opt_1 = int(i)
-                opt_2 = int(j)           
-                value_opt_1 = copy.deepcopy(seed[0][opt_1])
-                value_opt_2 = copy.deepcopy(seed[0][opt_2])           
-                if (full_swap == False):
-                    best_route[0][opt_1] = value_opt_2
-                    best_route[0][opt_2] = value_opt_1    
-                elif (full_swap == True):
-                    best_route[0][opt_1:opt_2+1] = list(reversed(best_route[0][opt_1:opt_2+1]))           
+                best_route[0][i:j+1] = list(reversed(best_route[0][i:j+1]))           
                 best_route[0][-1]  = best_route[0][0]              
                 for k in range(0, len(city_list[0])-1):
                     m = k + 1
@@ -54,4 +46,4 @@ def local_search_2_opt(Xdata, city_tour, full_swap = True, recursive_seeding = 1
 X = pd.read_csv('Python-MH-Local Search-2-opt-Dataset-01.txt', sep = '\t') #17 cities => Optimum = 2085
 
 cities = [[   1,  2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,   1   ], 4722]
-ls2opt = local_search_2_opt(X, city_tour = cities, full_swap = True, recursive_seeding = 11)
+ls2opt = local_search_2_opt(X, city_tour = cities, recursive_seeding = 11)
